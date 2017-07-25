d3d6afaa88a54fa461cbec84ef80dcfcf625d5c6
drilldripper <gs16012@s.inf.shizuoka.ac.jp>
2017-07-25 22:12:49 +0900

    Add User controllers

--- api-server/config/routes.rb
+++ api-server/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  post 'users/create'
+
+  get 'users/show'
+
+  patch 'users/update'
+
+  delete 'users/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
