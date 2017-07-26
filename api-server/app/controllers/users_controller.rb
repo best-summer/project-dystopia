f4c3b113f3a9639e108ede917295759b08a1635e
drilldripper <gs16012@s.inf.shizuoka.ac.jp>
2017-07-26 20:20:12 +0900

    Add client_id to user model

--- api-server/app/controllers/users_controller.rb
+++ api-server/app/controllers/users_controller.rb
@@ -1,16 +1,43 @@
 class UsersController < ApplicationController
   def index
-  end
-
-  def create
+    @users = User.all
+    render json: @users
   end
 
   def show
+    # @user = User.find(params[:id])
+    @user = User.find_by(name: params[:id])
+
+    render json: @user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  # PATCH/PUT /books/1
   def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
+  # DELETE /books/1
   def destroy
+    @book.destroy
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
+
--- api-server/db/schema.rb
+++ api-server/db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170725103921) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "client_id"
     t.string   "name"
     t.integer  "score"
     t.integer  "billing"
