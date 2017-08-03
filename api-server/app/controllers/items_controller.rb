60a81c125a4cac326345d0fda415000dabbf8717
drilldripper <gs16012@s.inf.shizuoka.ac.jp>
2017-08-03 20:49:29 +0900

    Fix error responses

--- api-server/app/controllers/items_controller.rb
+++ api-server/app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
   def index
-    # 全ユーザの情報を表示する
+    # 全アイテムの情報を表示する(デバッグ用)
     @items = Item.all.as_json
     render json: @items
   end
 
   def show
+    #  POST /users/:name/items?login_key
+    # ユーザの所有しているアイテムを表示する
     @user = User.find_by(name: params[:name])
     @items = @user.item
     if @user.authenticate_only_login_key?(params)
@@ -16,27 +18,29 @@ class ItemsController < ApplicationController
   end
 
   def create
+    #  POST /users/:name/items
+    # ユーザのアイテムを作成する
     @user = User.find_by(name: params[:name])
     @item = Item.new(name: params[:item][:name], value: params[:item][:value],
                      user_id: @user.id)
     if @user.authenticate_only_login_key?(params) && @item.save
-      render json: @item.as_json
+      render 'create', formats: 'json'
     else
       render json: {status: 'ng', message: 'Wrong login key'}
     end
   end
 
-  #  PATCH /users/:name/items?login_key
-  # アイテム情報の更新を行う
+  #  PATCH /users/:name/items
+  # ユーザのアイテム情報の更新をする
   def update
-    p params
     @user = User.find_by(name: params[:name])
     @item = @user.item.find_by(name: params[:item][:name])
-
+    if @item.nil?
+      render json: {status: 'ng', message: 'user_name or item_name is wrong'}
+      return
+    end
     for param in params[:item]
-      p param
-      p params[:item][param]
-      p @item[param] = params[:item][param]
+      @item[param] = params[:item][param]
     end
 
     if @user.authenticate_only_login_key?(params) && @item.save
@@ -45,8 +49,6 @@ class ItemsController < ApplicationController
       render json: {status: 'ng', message: 'Wrong login key'}
     end
   end
-
-
 
   def destroy
   end
--- api-server/app/views/items/create.json.jbuilder
+++ api-server/app/views/items/create.json.jbuilder
@@ -1,6 +1,2 @@
-json.items do
-  json.array! @items do |item|
-    json.name item.name
-    json.value item.value
-  end
-end
+json.name @item.name
+json.value @item.value
