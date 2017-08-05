88f83d9d9303863cb7bdea3038c7641e6c81c2e7
yurafuca <madokawaii@gmail.com>
2017-08-06 02:06:08 +0900

    Use classes for user.status and user.resuts

--- websocket-server/rails.js
+++ websocket-server/rails.js
@@ -45,19 +45,27 @@ class Users {
   constructor(user_name, login_key) {
     this.user_name = user_name;
     this.login_key = login_key;
+    this.status = new Status(user_name, login_key);
     this.items = new Items(user_name, login_key);
+    this.results = new Results(user_name, login_key);
   }
 
   list(callback) {
-    var options = {
-      url: END_POINT + `users`,
-    };
+    var options = { url: END_POINT + `users` };
     request.get(options, function (error, response, body) {
       callback(body);
     });
   }
+}
 
-  status(callback) {
+
+class Status {
+  constructor(user_name, login_key) {
+    this.user_name = user_name;
+    this.login_key = login_key;
+  }
+
+  get(callback) {
     var options = {
       url: END_POINT +
            `users/` + this.user_name +
@@ -67,37 +75,6 @@ class Users {
     request.get(options, function (error, response, body) {
       callback(body);
     });
-  }
-
-  results(new_props, callback) {
-    var self = this;
-    var get = function(callback) {
-      var options = {
-        url: END_POINT +
-             `users/` + self.user_name +
-             `/results?login_key=` + self.login_key,
-      };
-      request.get(options, function (error, response, body) {
-        callback(body);
-      });
-    }
-    var set = function(new_props, callback) {
-      var options = {
-        url: END_POINT + `users/` + self.user_name + `/results`,
-        json: {
-          score: new_props.score,
-          vs: new_props.vs,
-          login_key: new_props.login_key
-        }
-      };
-      request.patch(options, function (error, response, body) {
-        callback(body);
-      });
-    }
-    if (new_props)
-      set(new_props, callback);
-    else
-      get(callback);
   }
 }
 
@@ -142,6 +119,39 @@ class Items {
       json: new_props
     };
     request.get(options, function (error, response, body) {
+      callback(body);
+    });
+  }
+}
+
+
+class Results {
+  constructor(user_name, login_key) {
+    this.user_name = user_name;
+    this.login_key = login_key;
+  }
+
+  get(callback) {
+    var options = {
+      url: END_POINT +
+      `users/` + self.user_name +
+      `/results?login_key=` + this.login_key,
+    };
+    request.get(options, function (error, response, body) {
+      callback(body);
+    });
+  }
+
+  set(new_props, callback) {
+    var options = {
+      url: END_POINT + `users/` + this.user_name + `/results`,
+      json: {
+        score: new_props.score,
+        vs: new_props.vs,
+        login_key: new_props.login_key
+      }
+    };
+    request.patch(options, function (error, response, body) {
       callback(body);
     });
   }
