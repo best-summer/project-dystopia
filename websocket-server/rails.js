07759695ab52c5748ff2dc3164b9ca1130fe83a6
yurafuca <madokawaii@gmail.com>
2017-08-06 01:57:07 +0900

    Fix rails.js

--- websocket-server/rails.js
+++ websocket-server/rails.js
@@ -1,3 +1,4 @@
+var request = require("request");
 var END_POINT = `https://best-summer-api.herokuapp.com/`
 
 module.exports = class Rails {
@@ -5,41 +6,46 @@ module.exports = class Rails {
     this.items = new Items();
   }
 
-  signup(props) {
+  static signup(props, callback) {
     var options = {
       url: END_POINT + `signup`,
       headers: { "Content-type": "application/json" },
-      json: props
+      json: {
+        name: props.name,
+        device_id: props.device_id
+      }
     };
     request.post(options, function (error, response, body) {
       console.log(body);
+      callback(new Users(body.user_name, body.login_key));
     });
   }
 
   // Rails.users();
   // Rails.users(`Nenecchi`).status();
-  // Rails.users(`Nenecchi`).items();
+  // Rails.users(`Nenecchi`).items(null);
   // Rails.users(`Nenecchi`).items({ name: `SuperBall` });
-  // Rails.users(`Nenecchi`).results();
+  // Rails.users(`Nenecchi`).results(null);
   // Rails.users(`Nenecchi`).results({ score: 200, vs: `win` });
-  users(user_name) {
-    if (user_name)
-      return new Users(user_name);
+  static users(user_name, login_key) {
+    if (user_name && login_key)
+      return new Users(user_name, login_key);
     else
       return (new Users()).list();
   }
 
   // Rails.items();
-  items() {
+  static items() {
     return (new Items()).list();
   }
 }
 
 
 class Users {
-  constructor(user_name) {
-    this.user_name = user_name
-    this.items = new Items(user_name);
+  constructor(user_name, login_key) {
+    this.user_name = user_name;
+    this.login_key = login_key;
+    this.items = new Items(user_name, login_key);
   }
 
   list(callback) {
@@ -57,22 +63,19 @@ class Users {
            `users/` + this.user_name +
            `/status?login_key=` + this.login_key,
     };
+    console.log(this.login_key);
     request.get(options, function (error, response, body) {
       callback(body);
     });
   }
 
-  items(new_props, callback) {
-    if (new_props)
-      this.items.set(new_props, callback);
-    else
-      this.items.get(callback);
-  }
-
   results(new_props, callback) {
+    var self = this;
     var get = function(callback) {
       var options = {
-        url: END_POINT + `users/` + self.user_name + `/results`,
+        url: END_POINT +
+             `users/` + self.user_name +
+             `/results?login_key=` + self.login_key,
       };
       request.get(options, function (error, response, body) {
         callback(body);
@@ -81,6 +84,11 @@ class Users {
     var set = function(new_props, callback) {
       var options = {
         url: END_POINT + `users/` + self.user_name + `/results`,
+        json: {
+          score: new_props.score,
+          vs: new_props.vs,
+          login_key: new_props.login_key
+        }
       };
       request.patch(options, function (error, response, body) {
         callback(body);
@@ -95,8 +103,9 @@ class Users {
 
 
 class Items {
-  constructor(user_name) {
+  constructor(user_name, login_key) {
     this.user_name = user_name;
+    this.login_key = login_key;
   }
 
   get(callback) {
@@ -112,8 +121,15 @@ class Items {
 
   set(new_props, callback) {
     var options = {
-      json: new_props
-      url: END_POINT + `users/` + this.user_name + `/items`,
+      url: END_POINT + 
+           `users/` + this.user_name +
+           `/items?login_key=` + this.login_key,
+      json: {
+        name: new_props.name,
+        login_key: new_props.login_key,
+        value: new_props.value,
+        number: new_props.number
+      }
     };
     request.post(options, function (error, response, body) {
       callback(body);
@@ -122,7 +138,7 @@ class Items {
 
   list(callback) {
     var options = {
-      url: END_POINT + `items`,
+      url: END_POINT + `/items`,
       json: new_props
     };
     request.get(options, function (error, response, body) {
